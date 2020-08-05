160a159bceb6c189eb8f7a12faffe40a3d2411c0
nelson-singleton <nsingleton@ymail.com>
2020-08-05 15:05:50 -0500

    Created function. Added listener to button

--- components/Article.js
+++ components/Article.js
@@ -114,3 +114,19 @@ const data = [
   Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
+function articleMaker(articleObj){
+  const article = document.createElement('div')
+  const title = document.createElement('h2')
+  const date = document.createElement('p')
+  const p1 = document.createElement('p')
+  const p2 = document.createElement('p')
+  const p3 = document.createElement('p')
+  const button = document.createElement('span')
+
+
+  button.addEventListener('click', event => {
+    article.classList.toggle('article-open')
+  })
+  return article
+
+}
